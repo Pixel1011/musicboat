47cbf82d4556fc90ba4d0af5efc4156f3caeca91
Pixel1011 <enderdragon1105@gmail.com>
2021-10-31 17:17:23 +0000

    fix reload

--- Bot-Source/commands/reload.js
+++ Bot-Source/commands/reload.js
@@ -1,8 +1,20 @@
 
 async function run(client, msg, args) {
   if (msg.author.id !== client.config.ownerid) return;
+  let command = args.join(" ").toLowerCase();
+  if(!client.commands[command]) return;
 
-  await client.boat.ReloadCommands(msg);
+  delete require.cache[require.resolve("./" + command)];
+  try {
+    let cmdFile = require("./" + command);
+    client.commands[command] = cmdFile;
+    cmdFile.aliases.forEach(alias => {
+      client.commands[alias] = cmdFile;  
+    });
+  } catch (e) {
+    msg.channel.send(`Failed to load ${command}\n ${e.stack}`);
+  }
+  msg.channel.send(`Reloaded \`\`${command}\`\``);
 }
 module.exports.data = {
   name: "reload",
