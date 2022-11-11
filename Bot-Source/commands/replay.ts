a511db11e29de302bb8e94ec224507668055b3b5
BuildTools <unconfigured@null.spigotmc.org>
2022-11-11 14:43:28 +0000

    replay command

---
+++ Bot-Source/commands/replay.ts
@@ -0,0 +1,28 @@
+import type { musicBot } from "../client";
+import { musicHelper } from "../utils/musicHelper";
+import type { UnifiedData } from "../utils/SlashUnifier";
+
+async function run(client: musicBot, data: UnifiedData) {
+  const music = new musicHelper(client, data.guild.id);
+  let player = music.getPlayer();
+  if (!player || (!player.queue.currentSong && !player.queue.lastSong)) return data.send(":x: **Bot has not previously played a song**"); // check responses later
+  if (player.playing && (player.queue || player.queue.currentSong)) {
+    // if playing, seek song back to 0
+    player.seek(0);
+    data.send(":musical_note: **Song progress reset** :track_previous:");
+  } else {
+    // if not playing, play last song
+    let lastSong = player.queue.lastSong;
+    player.queue.songs.unshift(lastSong);
+    player.play(lastSong.track);
+    data.send(":musical_note: **Song progress reset** :track_previous:");
+  }
+}
+export const data = {
+  name: "replay",
+  description: "Resets the current progress of the song",
+  aliases: [""],
+  hide: false,
+  arguments: [],
+  run: run
+};
